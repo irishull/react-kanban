2595fb30c97827c245798bd67317a7b99f4ced84
Joe Hull <josephghull@gmail.com>
2018-06-04 19:07:04 -0400

    Move notes constant to App.jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+
+import uuid from 'uuid';
 import Notes from './Notes';
+
+const notes = [
+    {
+        id: uuid.v4(),
+        task: 'Learn React'
+    },
+    {
+        id: uuid.v4(),
+        task: 'Do something'
+    }
+];
+
 
 export default () => (
     <div>
         <button onClick={() => console.log('add note')}>+</button>
-        <Notes />
+        <Notes notes={notes} />
     </div>
 
 );
